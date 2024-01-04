4add9448d6ef8e809f4b9220aae4ed4f0365b582
Kevin Harrington <kharrington@bancroftschool.org>
2024-01-04 14:35:19 -0500

    adding file loader

--- ShrinkAndLayout.groovy
+++ ShrinkAndLayout.groovy
@@ -1,1 +1,16 @@
+import javax.swing.JFileChooser
+
 // code here
+
+JFileChooser f = new JFileChooser();
+f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+f.showSaveDialog(new File(System.getProperty("user.home")+"/Downloads/"));
+
+double scale = 0.25
+
+def dir = f.getSelectedFile()
+System.out.println(dir);
+
+File[] listOfFiles = dir.listFiles();
+
+println listOfFiles
