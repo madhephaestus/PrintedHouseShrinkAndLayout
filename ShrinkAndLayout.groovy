f101c0dba54876d9ab7f5ab724bb7a01a1b19db7
Kevin Harrington <kharrington@bancroftschool.org>
2024-01-23 10:10:02 -0500

    shrink and layout

--- ShrinkAndLayout.groovy
+++ ShrinkAndLayout.groovy
@@ -1,16 +1,33 @@
 import javax.swing.JFileChooser
 
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
+
+import eu.mihosoft.vrl.v3d.CSG
+
 // code here
 
-JFileChooser f = new JFileChooser();
+JFileChooser f = new JFileChooser(new File(System.getProperty("user.home")+"/Downloads/all/"));
 f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-f.showSaveDialog(new File(System.getProperty("user.home")+"/Downloads/"));
 
-double scale = 0.25
+f.showSaveDialog(null);
+
+double scale = 1.0/3.5
 
 def dir = f.getSelectedFile()
 System.out.println(dir);
 
 File[] listOfFiles = dir.listFiles();
 
-println listOfFiles
+ArrayList<CSG> stls= []
+
+for(int i=0;i<listOfFiles.length;i++) {
+	
+	File file=listOfFiles[i];
+	println "Loading "+file
+	CSG get = Vitamins.get(file)
+	get=get.scale(scale)
+	get.setName(file.getName())
+	stls.add(get)
+}
+
+return stls
